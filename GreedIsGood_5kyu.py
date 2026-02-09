00f2203838b39705f5da77eb5917173d75156207
Igor <150297879+igormain@users.noreply.github.com>
2026-02-09 20:03:29 +0400

    Add files via upload

---
+++ GreedIsGood_5kyu.py
@@ -0,0 +1,10 @@
+def score(dice):
+    scores_3same = [1000, 200, 300, 400, 500, 600]
+    scores_single = [100 ,   0,   0,   0,  50,   0]
+    
+    sum = 0
+    for i in range(1,7):
+        count_i = dice.count(i)
+        sum += (count_i // 3) * scores_3same[i-1] + (count_i % 3) * scores_single[i-1]
+            
+    return sum
