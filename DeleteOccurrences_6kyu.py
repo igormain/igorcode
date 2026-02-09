4a64f6915315522ca8ae12dc0c3a93fe82755147
Igor <150297879+igormain@users.noreply.github.com>
2026-02-10 01:30:43 +0400

    Delete DeleteOccurrences_6kyu.py

--- DeleteOccurrences_6kyu.py
+++
@@ -1,8 +0,0 @@
-def delete_nth(order,max_e):
-    exit = [], counts = {}
-    for i in order:
-        current_count = counts.get(i, 0)
-        if current_count < max_e:
-            exit.append(i)
-            counts[i] = current_count + 1
-    return exit
